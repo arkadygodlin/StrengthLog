dbcd810373256b2cb370b35f44e671f5c2e6a04a
Arkady Godlin <arkady.godlin@cortica.com>
2015-10-23 20:46:24 +0300

    Add sample App

---
+++ app/src/main/java/com/strengthlog/MainActivity.java
@@ -0,0 +1,144 @@
+package com.strengthlog;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks
+{
+
+  /**
+   * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+   */
+  private NavigationDrawerFragment mNavigationDrawerFragment;
+
+  private CharSequence mTitle;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.navigation_drawer);
+    mTitle = getTitle();
+
+    // Set up the drawer.
+    mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+  }
+
+  @Override
+  public void onNavigationDrawerItemSelected(int position)
+  {
+    // update the main content by replacing fragments
+    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+    Fragment newFragment = null;
+
+    switch (position)
+    {
+      case 0:
+        newFragment = PlaceholderFragment.newInstance(0);
+        mTitle = getString(R.string.title_section0);
+        break;
+      case 1:
+        newFragment = PlaceholderFragment.newInstance(0);
+        mTitle = getString(R.string.title_section1);
+        break;
+      default:
+    }
+    transaction.replace(R.id.container, newFragment);
+    transaction.commit();
+  }
+
+  public void onSectionAttached(int number)
+  {
+
+    //TODO remove this, it doesn't do shit, used only in DUMMY FRAGMNET
+
+    switch (number)
+    {
+      case 0:
+        mTitle = getString(R.string.title_section0);
+        break;
+      case 1:
+        mTitle = getString(R.string.title_section1);
+        break;
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+    if (!mNavigationDrawerFragment.isDrawerOpen())
+    {
+      // Only show items in the action bar relevant to this screen
+      // if the drawer is not showing. Otherwise, let the drawer
+      // decide what to show in the action bar.
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+    }
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+    return super.onOptionsItemSelected(item);
+  }
+
+  /**
+   * A placeholder fragment containing a simple view.
+   */
+  public static class PlaceholderFragment extends Fragment
+  {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public PlaceholderFragment()
+    {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceholderFragment newInstance(int sectionNumber)
+    {
+      PlaceholderFragment fragment = new PlaceholderFragment();
+      Bundle args = new Bundle();
+      args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+      fragment.setArguments(args);
+      return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+      View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+      return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+      super.onAttach(activity);
+      ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+  }
+
+
+}
